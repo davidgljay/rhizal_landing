2a15001a337ab71962866b3b86ce37522e07cfeb
David Jay <davidgljay@gmail.com>
2025-11-14 20:05:05 -0800

    Updating landign page.

--- _site/js/custom.js
+++ _site/js/custom.js
@@ -1,23 +1,45 @@
 (function($) {
     "use strict"; // Start of use strict
-
-	const path = window.location.pathname;
-    const link = document.querySelector( `.navbar-default li a[href="${path}"]` );
-
-    $(link).addClass('active');
-
-    $('#organizingForm').submit(function(e){
-        e.preventDefault();
+    var emailSubmitted = false;
+    var email = '';
+    $('#email-form').submit(function(event) {
+        event.preventDefault();
+        email = $('#email').val();
+        if (!email || !email.match(/@/)) {
+        alert('Please enter a valid email address.');
+        return;
+        }    
+        $('#email-form').hide();
+        $('#details-form').show();
+        emailSubmitted = true;
+        var formData = new FormData(this);
         $.ajax({
-            url: 'https://hook.us1.make.com/fq8rntt8lxkg80m6omalzlcwmu6o1bpo',
-            type: 'post',
-            data:$('#organizingForm').serialize(),
-            success:function(){
-                $('#organizingArea').hide()
-                $('#organizingThanks').show()
-                console.log('success')
-            }
+        url: 'https://hooks.zapier.com/hooks/catch/14500435/u326m5w/',
+        type: 'post',
+        data: formData,
+        contentType: false,
+        processData: false,
         });
+        
+    });
+    $('#details-form form').submit(function(event) {
+        event.preventDefault();
+        if (!$('#description').val()) {
+            alert('Please describe how you plan to use Rhizal.');
+            return;
+        }
+        var formData = new FormData(this);
+        formData.append('Email', email);
+
+        $.ajax({
+            url: 'https://hooks.zapier.com/hooks/catch/14500435/u326m5w/',
+            type: 'post',
+            data: formData,
+            contentType: false,
+            processData: false,
+        });
+        $('#details-form').hide();
+        $('#thanks').show();
     });
 
 })(jQuery); // End of use strict
